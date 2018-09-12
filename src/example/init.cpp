c9165ad9e2d314d67262d36b7fe972a13c6a3178
Carl Pearson <pearson@illinois.edu>
2018-09-11 23:34:16 -0400

    register version string

--- src/example/init.cpp
+++ src/example/init.cpp
@@ -10,26 +10,35 @@
 DECLARE_FLAG_bool(example);
 DEFINE_FLAG_bool(example, false, "an example flag");
 
+// register an option statically
 SCOPE_REGISTER_OPTS(
-    clara::Opt(FLAG(example))["-e1"]("example flag (from Example|Scope)"),
-    clara::Opt(FLAG(example))["-e2"]("another flag (from Example|Scope)")
+    clara::Opt(FLAG(example))["-e1"]("static flag (from Example|Scope)")
 );
+
+void example_before_init() {
+    // Create a version string and tell scope about it
+    // These values are defined in example_scope/config.hpp.in
+    RegisterVersionString(
+        version(SCOPE_PROJECT_NAME,
+                             SCOPE_VERSION,
+                             SCOPE_GIT_REFSPEC,
+                             SCOPE_GIT_HASH,
+                             SCOPE_GIT_LOCAL_CHANGES)
+    );
+
+    RegisterOpt( clara::Opt(FLAG(example))["-e2"]("programmatic flag (from Example|Scope)") );
+}
 
 int example_init() {
     
     // Do something with some command line flags
     // parse(argc, argv);
-    if (FLAG(version)) {
-        // Print a version string
-        // These values are defined in example_scope/config.hpp.in
-        std::cout << version(SCOPE_PROJECT_NAME,
-                             SCOPE_VERSION,
-                             SCOPE_GIT_REFSPEC,
-                             SCOPE_GIT_HASH,
-                             SCOPE_GIT_LOCAL_CHANGES) << std::endl;
+    if (FLAG(example)) {
 
-        // return 0 so that the Scope init infrastructure continues
-        return 0;
+        LOG(critical, "Example flag was set, so exiting!");
+
+        // signal that initialization should fail
+        return 1;
     }
 
     // Do some other initialization
@@ -39,5 +48,6 @@ int example_init() {
     return 0;
 }
 
+SCOPE_REGISTER_BEFORE_INIT(example_before_init);
 SCOPE_REGISTER_INIT(example_init);
 
